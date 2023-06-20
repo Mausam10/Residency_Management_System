e902839bb7871c430ed1ed0585ff8561e90f6b2f
Prashant Adhikari <subash96240@gmail.com>
2023-06-20 07:19:26 +0545

    guard notice form

---
+++ griha_UI/G_notices3.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class G_notices3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(G_notices3))
+        Panel1 = New Panel()
+        Button_back = New Button()
+        Button_notices = New Button()
+        Button_Workers = New Button()
+        Button_Visitors = New Button()
+        button_resident = New Button()
+        PictureBox2 = New PictureBox()
+        Label1 = New Label()
+        PictureBox1 = New PictureBox()
+        Panel1.SuspendLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = SystemColors.MenuText
+        Panel1.Controls.Add(Button_back)
+        Panel1.Controls.Add(Button_notices)
+        Panel1.Controls.Add(Button_Workers)
+        Panel1.Controls.Add(Button_Visitors)
+        Panel1.Controls.Add(button_resident)
+        Panel1.Controls.Add(PictureBox2)
+        Panel1.Location = New Point(1, 1)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(500, 1080)
+        Panel1.TabIndex = 11
+        ' 
+        ' Button_back
+        ' 
+        Button_back.BackColor = Color.Black
+        Button_back.BackgroundImageLayout = ImageLayout.None
+        Button_back.Font = New Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Button_back.ForeColor = Color.White
+        Button_back.Location = New Point(121, 771)
+        Button_back.Name = "Button_back"
+        Button_back.Size = New Size(248, 43)
+        Button_back.TabIndex = 14
+        Button_back.Text = "Back to Login"
+        Button_back.UseVisualStyleBackColor = False
+        ' 
+        ' Button_notices
+        ' 
+        Button_notices.BackColor = Color.Black
+        Button_notices.BackgroundImageLayout = ImageLayout.None
+        Button_notices.Font = New Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Button_notices.ForeColor = Color.White
+        Button_notices.Location = New Point(121, 648)
+        Button_notices.Name = "Button_notices"
+        Button_notices.Size = New Size(248, 43)
+        Button_notices.TabIndex = 12
+        Button_notices.Text = "Notices"
+        Button_notices.UseVisualStyleBackColor = False
+        ' 
+        ' Button_Workers
+        ' 
+        Button_Workers.BackColor = Color.Black
+        Button_Workers.BackgroundImageLayout = ImageLayout.None
+        Button_Workers.Font = New Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Button_Workers.ForeColor = Color.White
+        Button_Workers.Location = New Point(121, 521)
+        Button_Workers.Name = "Button_Workers"
+        Button_Workers.Size = New Size(248, 43)
+        Button_Workers.TabIndex = 11
+        Button_Workers.Text = "Workers"
+        Button_Workers.UseVisualStyleBackColor = False
+        ' 
+        ' Button_Visitors
+        ' 
+        Button_Visitors.BackColor = Color.Black
+        Button_Visitors.BackgroundImageLayout = ImageLayout.None
+        Button_Visitors.Font = New Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Button_Visitors.ForeColor = Color.White
+        Button_Visitors.Location = New Point(121, 403)
+        Button_Visitors.Name = "Button_Visitors"
+        Button_Visitors.Size = New Size(248, 43)
+        Button_Visitors.TabIndex = 9
+        Button_Visitors.Text = "Visitors"
+        Button_Visitors.UseVisualStyleBackColor = False
+        ' 
+        ' button_resident
+        ' 
+        button_resident.BackColor = Color.Black
+        button_resident.BackgroundImageLayout = ImageLayout.None
+        button_resident.Font = New Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        button_resident.ForeColor = Color.White
+        button_resident.Location = New Point(121, 289)
+        button_resident.Name = "button_resident"
+        button_resident.Size = New Size(248, 43)
+        button_resident.TabIndex = 8
+        button_resident.Text = "Residents"
+        button_resident.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(3, 3)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(420, 225)
+        PictureBox2.TabIndex = 0
+        PictureBox2.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Microsoft Sans Serif", 24F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label1.Location = New Point(1003, 177)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(418, 46)
+        Label1.TabIndex = 13
+        Label1.Text = "Notices of Residency"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.BackgroundImageLayout = ImageLayout.None
+        PictureBox1.BorderStyle = BorderStyle.Fixed3D
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(502, 1)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(201, 222)
+        PictureBox1.TabIndex = 12
+        PictureBox1.TabStop = False
+        ' 
+        ' G_notices3
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(1902, 1033)
+        Controls.Add(Panel1)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Name = "G_notices3"
+        Text = "G_notices3"
+        Panel1.ResumeLayout(False)
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button_back As Button
+    Friend WithEvents Button_notices As Button
+    Friend WithEvents Button_Workers As Button
+    Friend WithEvents Button_Visitors As Button
+    Friend WithEvents button_resident As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
