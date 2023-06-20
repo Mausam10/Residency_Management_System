ae12d3264c3eda8fecd7e81d0ab60f34daa8e968
Sushan Poudel <sushanpoudel75@gmail.com>
2023-06-20 17:17:30 +0545

    unique class created for database

--- griha_UI/S_residents3.vb
+++ griha_UI/S_residents3.vb
@@ -2,6 +2,9 @@
 Imports System.Data
 
 Public Class S_residents3
+    Dim query As String
+    Dim tempDt As DataTable
+
     Private Sub Button_Events_Click(sender As Object, e As EventArgs) Handles Button_Events.Click
         Me.Hide()
         S_events3.Show()
@@ -28,10 +31,6 @@ Public Class S_residents3
     End Sub
 
     Private Sub Button_add_Click(sender As Object, e As EventArgs) Handles Button_add.Click
-        Dim con As SqlConnection
-        Dim cmd As SqlCommand = New SqlCommand
-        con = New SqlConnection("Data Source=LAPTOP-390QHVCB;Initial Catalog=griha;Integrated Security=True;TrustServerCertificate=True")
-        cmd.CommandType = System.Data.CommandType.Text
 
         Dim Username As String
         Dim Password, First_Name, Middle_Name, Last_Name, Phone1, Phone2, Email, Apartment_id As String
@@ -44,42 +43,40 @@ Public Class S_residents3
         Phone2 = TextBox_phoneno2.Text
         Email = TextBox_email.Text
         Apartment_id = TextBox_apartmentid.Text
-        cmd.Connection = con
-        con.Open()
-        cmd.CommandText = "Insert into residents(Username,Password,First_Name,Middle_Name, Last_Name, Phone1, Phone2, Email, Apartment_id) values(@Username,@Password,@First_Name,@Middle_Name,@Last_Name,@phone1,@phone2,@email,@Apartment_id)"
-        cmd.Parameters.AddWithValue("@Username", Username)
-        cmd.Parameters.AddWithValue("@Password", Password)
-        cmd.Parameters.AddWithValue("@First_Name", First_Name)
-        cmd.Parameters.AddWithValue("@Middle_Name", Middle_Name)
-        cmd.Parameters.AddWithValue("@Last_Name", Last_Name)
-        cmd.Parameters.AddWithValue("@Phone1", Phone1)
-        cmd.Parameters.AddWithValue("@Phone2", Phone2)
-        cmd.Parameters.AddWithValue("@Email", Email)
-        cmd.Parameters.AddWithValue("@Apartment_id", Apartment_id)
-        cmd.ExecuteNonQuery()
-        MsgBox("Successfully added")
+
+        query = "Insert into residents(Username,Password,First_Name,Middle_Name, Last_Name, Phone1, Phone2, Email, Apartment_id) values('" & Username & "', ' " & Password & " ' ,' " & First_Name & " ',' " & Middle_Name & " ',' " & Last_Name & " ',' " & Phone1 & " ' ,' " & Phone2 & " ',' " & Email & " ',' " & Apartment_id & " ')"
+        If (grihaDb.executeMySql(query)) Then
+            MsgBox("Record Added")
+        End If
         populate()
+
+
     End Sub
 
     Private Sub populate()
-        Dim con As SqlConnection
-        con = New SqlConnection("Data Source=LAPTOP-390QHVCB;Initial Catalog=griha;Integrated Security=True;TrustServerCertificate=True")
-        con.Open()
-        Dim query = "select * from residents"
-        Dim adapter As SqlDataAdapter
-        adapter = New SqlDataAdapter(query, con)
-        Dim builder As SqlCommandBuilder
-        builder = New SqlCommandBuilder(adapter)
-        Dim ds As DataSet
-        ds = New DataSet
-        adapter.Fill(ds)
-        DataDVG.DataSource = ds.Tables(0)
-        con.Close()
+        query = "select * from residents"
+        tempDt = grihaDb.generateTable(query)
+        If (tempDt Is Nothing) Then
+            MsgBox("No record")
+
+        Else
+            DataDVG.DataSource = tempDt
+        End If
+
     End Sub
 
 
 
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataDVG.CellContentClick
+        populate()
+    End Sub
+
+    Private Sub button_resident_Click(sender As Object, e As EventArgs) Handles button_resident.Click
+
+    End Sub
+
+    Private Sub S_residents3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        grihaDb.connect()
         populate()
     End Sub
 End Class
---
+++ griha_UI/grihaDb.vb
@@ -0,0 +1,49 @@
+﻿Imports Microsoft.Data.SqlClient
+Imports System.Data
+
+Public Class grihaDb
+    Shared con As SqlConnection
+    Shared cmd As SqlCommand
+    Shared sda As SqlDataAdapter
+    Shared dt As DataTable
+
+    Shared Sub connect()
+        con = New SqlConnection("Data Source=LAPTOP-390QHVCB;Initial Catalog=griha;Integrated Security=True;TrustServerCertificate=True")
+    End Sub
+
+    Shared Function executeMySql(ByVal sqlQuery As String) As Boolean
+        Try
+            cmd = New SqlCommand(sqlQuery, con)
+            con.Open()
+            cmd.ExecuteNonQuery()
+            con.Close()
+            Return True
+
+        Catch ex As Exception
+            con.Close()
+            MsgBox(ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Shared Function generateTable(ByVal sqlQuery As String) As DataTable
+        sda = New SqlDataAdapter
+        dt = New DataTable
+        Try
+            cmd = New SqlCommand(sqlQuery, con)
+            con.Open()
+            sda.SelectCommand = cmd
+            sda.Fill(dt)
+            con.Close()
+            Return dt
+
+        Catch ex As Exception
+            con.Close()
+            MsgBox(ex.Message)
+            dt = Nothing
+            Return dt
+        End Try
+
+    End Function
+
+End Class
