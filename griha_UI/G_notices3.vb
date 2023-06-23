fa10a5900479e5aa2f9a4b04a3bfae0006d49d0d
Mausam Pariyar <weatherofficial100@gmail.com>
2023-06-23 16:49:51 +0545

    guard_notice form error fixed

--- griha_UI/G_notices3.vb
+++ griha_UI/G_notices3.vb
@@ -2,4 +2,24 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub button_resident_Click(sender As Object, e As EventArgs) Handles button_resident.Click
+        Me.Hide()
+        G_residents3.Show()
+    End Sub
+
+    Private Sub Button_Workers_Click(sender As Object, e As EventArgs) Handles Button_Workers.Click
+        Me.Hide()
+        G_workers3.Show()
+    End Sub
+
+    Private Sub Button_back_Click(sender As Object, e As EventArgs) Handles Button_back.Click
+        Me.Hide()
+        Form_UI.Show()
+    End Sub
+
+    Private Sub Button_Visitors_Click(sender As Object, e As EventArgs) Handles Button_Visitors.Click
+        Me.Hide()
+        G_visitors3.Show()
+    End Sub
 End Class
